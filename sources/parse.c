9a833717cc14c0d88d40d8a1864fada1793021c7
Noah Mattos Oudejans <nmattos-@f0r4s11.codam.nl>
2024-12-02 16:11:11 +0100

    Basic outline for parsing the input string

---
+++ sources/parse.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattos- <nmattos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 10:55:22 by nmattos-          #+#    #+#             */
+/*   Updated: 2024/12/02 13:55:37 by nmattos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/parse.h"
+
+t_command	*parse_user_input(char *input)
+{
+	t_command	*commands;
+	t_variable	*variables;
+	char		**split_input;
+	int			i;
+
+	// tokenize user input
+	split_input = ft_split(input, ' ');
+	i = 0;
+	while (split_input[i] != NULL)
+	{
+		// check token for variable, if variable, parse and add to *variables
+		if (is_variable(split_input[i]))
+		{
+			parse_variable(split_input[i], &variables);
+		}
+		// check token for command
+		else if (is_command(split_input[i]))
+		{
+			// if command, parse and add to *commands
+			// if HERE_DOC (<<), readlines until EOF
+			// if TEXTFILE (<), set *input/*output to filename
+			// if PIPE (|), set in/out accordingly
+		}
+		i++;
+	}
+	return (commands);
+}
