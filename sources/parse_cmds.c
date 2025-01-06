b3836f047ab15e21c83e94ada23b42cb19d42223
Noma <noahmattosoudejans@icloud.com>
2025-01-06 12:29:09 +0100

    Fixed commands that require a string as option, such as cd or grep

--- sources/parse_cmds.c
+++ sources/parse_cmds.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parse_cmds.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/02 10:46:46 by nmattos-          #+#    #+#             */
-/*   Updated: 2024/12/20 12:55:06 by nmattos-         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   parse_cmds.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nmattos- <nmattos-@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/12/02 10:46:46 by nmattos-      #+#    #+#                 */
+/*   Updated: 2025/01/06 12:28:12 by nmattos       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,94 @@ int	is_command(char *cmd)
 	return (found);
 }
 
+static bool	is_special(char *command)
+{
+	if (ft_strncmp(command, "grep", 5) == 0 || ft_strncmp(command, "fgrep", 6) == 0
+		|| ft_strncmp(command, "egrep", 6) == 0 || ft_strncmp(command, "awk", 4) == 0
+		|| ft_strncmp(command, "sed", 4) == 0 || ft_strncmp(command, "find", 4) == 0
+		|| ft_strncmp(command, "cd", 3) == 0)
+		return (true);
+	return (false);
+}
+
+static int parse_string(char **input, int *i, char **options)
+{
+	size_t	size;
+	int		j;
+	char	*first;
+	char	*second;
+
+	size = 0;
+	j = *i;
+
+	first = ft_strchr(input[j], '\"');
+	second = ft_strchr(first + 1, '\"');
+	if (first != second && second != NULL)
+	{
+		size = second - first;
+		*options = ft_calloc((size + 1), sizeof(char));
+		if (*options == NULL)
+			return (FAIL);
+		ft_strlcpy(*options, first + 1, size);
+	}
+	size = ft_strlen(first);
+	if (second == NULL)
+	{
+		while (input[j + 1] != NULL)
+		{
+			if (ft_strchr(input[j + 1], '\"') != NULL)
+			{
+				second = ft_strchr(input[j + 1], '\"');
+				size += (second - input[j + 1]) + 1;
+				*options = ft_calloc((size + 1), sizeof(char));
+				if (*options == NULL)
+					return (FAIL);
+				ft_strlcat(*options, first + 1, size);
+				(*i)++;
+				while (*i < j + 1)
+				{
+					ft_strlcat(*options, " ", size);
+					ft_strlcat(*options, input[*i], size);
+					(*i)++;
+				}
+				ft_strlcat(*options, " ", size);
+				ft_strlcat(*options, input[*i], size);
+				break ;
+			}
+			size += ft_strlen(input[j + 1]) + 1;
+			j++;
+		}
+	}
+	return (SUCCESS);
+}
+
+static int special_parse_options(char **input, int *i, char **options)
+{
+	size_t		size;
+	int			j;
+
+	size = 0;
+	j = *i;
+	if (input[j + 1] == NULL)
+		return (SUCCESS);
+
+	if (ft_strchr(input[j + 1], '\"') != NULL)
+	{
+		(*i)++;
+		if (parse_string(input, i, options) == FAIL)
+			return (FAIL);
+		return (SUCCESS);
+	}
+
+	size = ft_strlen(input[j + 1]);
+	*options = ft_calloc((size + 1), sizeof(char));
+	if (*options == NULL)
+		return (FAIL);
+	ft_strlcpy(*options, input[j + 1], size + 1);
+	(*i)++;
+	return (SUCCESS);
+}
+
 /*	Parse the options of the command.
  *
  *	input:		the user input.
@@ -122,7 +210,12 @@ int	parse_command(char **input, t_command **cmds, int *i)
 
 	command = input[*i];
 	options = NULL;
-	if (parse_options(input, i, &options) == FAIL)
+	if (is_special(command))
+	{
+		if (special_parse_options(input, i, &options) == FAIL)
+			return (FAIL);
+	}
+	else if (parse_options(input, i, &options) == FAIL)
 		return (FAIL);
 	new_cmd = cmd_new(command, options);
 	if (options != NULL)
