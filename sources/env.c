40e62ac12b899cfe36c7483bc2af7b7e96b48605
Ivan Riadynskyi <iriadyns@f1r6s2.codam.nl>
2025-01-26 16:18:17 +0100

    add env_list from structure

--- sources/env.c
+++ sources/env.c
@@ -6,7 +6,7 @@
 /*   By: iriadyns <iriadyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 13:56:48 by iriadyns          #+#    #+#             */
-/*   Updated: 2025/01/06 09:46:23 by iriadyns         ###   ########.fr       */
+/*   Updated: 2025/01/26 16:10:20 by iriadyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 extern char	**environ;
 
-int	env(void)
+int	env(t_env *env_list)
 {
-	int	i;
-
-	i = 0;
-	while (environ[i])
+	t_env	*tmp = env_list;
+	while (tmp)
 	{
-		ft_putstr_fd(environ[i], STDOUT_FILENO);
-		ft_putstr_fd("\n", STDOUT_FILENO);
-		i++;
+		if (tmp->value)
+			printf("%s=%s\n", tmp->name, tmp->value);
+		else
+			printf("%s=\n", tmp->name);
+		tmp = tmp->next;
 	}
-	return (0);
+	return 0;
 }
+
