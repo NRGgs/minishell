a11eb37d18ec44bb37380289e55fb5147e4583de
Noah Mattos Oudejans <nmattos-@f0r4s15.codam.nl>
2025-02-12 13:53:58 +0100

    Fixed echo with single quotes still printing variables

--- sources/echo.c
+++ sources/echo.c
@@ -3,34 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iriadyns <iriadyns@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 13:21:35 by iriadyns          #+#    #+#             */
-/*   Updated: 2025/02/11 15:48:43 by iriadyns         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:52:32 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static void	print_arg_with_env(t_env *env_list, char *arg)
+/*	Removes the delimiter from a string.
+ *
+ *	str:		The string to remove the delimiter from.
+ *	delimiter:	The character to remove.
+ *
+ *	Return:		A new string without the delimiter.
+ */
+static char	*remove_delimiter(char *str, char delimiter)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+	new_str = malloc(ft_strlen(str));
+	if (new_str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != delimiter)
+		{
+			new_str[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	new_str[j] = '\0';
+	return (new_str);
+}
+
+static int	print_arg_with_env(t_env *env_list, char *arg, char quote)
 {
 	t_env	*var;
+	char	*str;
 
-	if (arg && arg[0] == '$')
+	str = remove_delimiter(arg, quote);
+	if (str == NULL)
+		return (-1);
+	if (str[0] == '$' && quote != '\'')
 	{
-		if (ft_strcmp("?", arg + 1) == 0)
+		if (ft_strcmp("?", str + 1) == 0)
 		{
 			ft_putnbr_fd(g_exit_status, STDOUT_FILENO);
-			return ;
+			return (0);
 		}
-		var = find_env_var(env_list, arg + 1);
+		var = find_env_var(env_list, str + 1);
 		if (var && var->value)
 			ft_putstr_fd(var->value, STDOUT_FILENO);
 		else
 			ft_putstr_fd("", STDOUT_FILENO);
 	}
 	else
-		ft_putstr_fd(arg, STDOUT_FILENO);
+		ft_putstr_fd(str, STDOUT_FILENO);
+	return (0);
 }
 
 int	check_echo_option(char *option)
@@ -57,7 +95,8 @@ int	echo(t_command *command)
 	i = 0;
 	while (args[i])
 	{
-		print_arg_with_env(command->env_list, args[i]);
+		if (print_arg_with_env(command->env_list, args[i], command->pattern[0]) == -1)
+			return (ft_free_split(args), 1);
 		if (args[i + 1])
 			ft_putstr_fd(" ", STDOUT_FILENO);
 		i++;
