4b5ed9db4de0ecce083e0807005faeba9eb663d5
Noah Mattos Oudejans <nmattos-@f1r5s1.codam.nl>
2025-02-21 13:44:18 +0100

    Accidentally removed two functions

--- sources/parse/redirections.c
+++ sources/parse/redirections.c
@@ -6,11 +6,67 @@
 /*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:01:53 by nmattos           #+#    #+#             */
-/*   Updated: 2025/02/21 12:45:35 by nmattos-         ###   ########.fr       */
+/*   Updated: 2025/02/21 13:44:02 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+/**
+ * @brief Handles textfile redirections.
+ *
+ * @param fn        Filename.
+ * @param redirect  Redirection type (">", ">>", "<").
+ * @param last      Linked list of commands.
+ * @param place     Place of 'redirect' in relation to the command (AFTER / BEFORE).
+ *
+ *	@return	SUCCESS (1) / FAIL (0).
+ */
+int	textfile_redirection(char *fn, char *redirect, t_command **last, int place)
+{
+	if (ft_strcmp(redirect, ">") == 0 || ft_strcmp(redirect, ">>") == 0)
+	{
+		(*last)->output = ft_strdup(fn);
+		if (ft_strcmp(redirect, ">>") == 0)
+			(*last)->out_type = APPEND;
+		else if (place == AFTER)
+			(*last)->out_type = TEXTFILE;
+		else if (place == BEFORE)
+			(*last)->in_type = TEXTFILE;
+		if ((*last)->output == NULL)
+			return (FAIL);
+	}
+	else if (ft_strcmp(redirect, "<") == 0)
+	{
+		(*last)->input = ft_strdup(fn);
+		if (place == AFTER)
+			(*last)->in_type = TEXTFILE;
+		else if (place == BEFORE)
+			(*last)->out_type = TEXTFILE;
+		if ((*last)->input == NULL)
+			return (FAIL);
+	}
+	return (SUCCESS);
+}
+
+/**
+ * Concatenates strings and adds a space between them.
+ *
+ * @param str: string to concatenate
+ * @param temp: string to concatenate to
+ * @param size: size of the new string
+ *
+ * @return new string.
+ */
+char	*join_strings(char *str, char *temp, size_t size)
+{
+	ft_strlcat(temp, str, size + 1);
+	if (str != NULL
+		&& str[ft_strlen(str) - 1] != '\"'
+		&& str[ft_strlen(str) - 1] != '\'')
+		ft_strlcat(temp, " ", size + 1);
+	return (temp);
+}
 
 /**
  * Handles textfile redirections.
