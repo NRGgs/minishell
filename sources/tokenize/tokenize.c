cc1894cc295096dd7edbf44d9cfbdfba3cfb66d5
Noma <noahmattosoudejans@icloud.com>
2025-02-25 14:51:24 +0100

    Added support for open quotes

--- sources/tokenize/tokenize.c
+++ sources/tokenize/tokenize.c
@@ -342,21 +342,44 @@ t_command	*get_commands(t_token *tokens)
 
 
 
+static char	check_open_quotes(char *str, size_t i)
+{
+	char	quote;
+	bool	escaped;
 
-
-
-
-
-
-
-
-
+	quote = '0';
+	escaped = false;
+	while (str[i] != '\0')
+	{
+		escaped = skip_escapes(str, &i, escaped);
+		if ((str[i] == '\'' || str[i] == '\"') && !escaped)
+		{
+			quote = str[i++];
+			while (str[i] != '\0' && str[i] != quote)
+				i++;
+			if (str[i] == '\0')
+				return (quote);
+		}
+		i++;
+	}
+	return ('0');
+}
 
 t_command	*parse_input(char *input)
 {
-	t_token	*tokens;
+	t_token		*tokens;
 	t_command	*commands;
+	char		quote;
 
+	if (input == NULL)
+		return (NULL);
+	quote = check_open_quotes(input, 0);
+	if (quote != '0')
+	{
+		input = read_till_quotes(quote, &input);
+		if (input == NULL)
+			return (NULL);
+	}
 	tokens = tokenize(input);
 	if (tokens == NULL)
 		return (NULL);
