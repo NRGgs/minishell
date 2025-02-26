fd38df66fccb9be79e1da2e7e1b8ebcf80aa879f
Noah Mattos Oudejans <nmattos-@f1r5s1.codam.nl>
2025-02-26 13:16:15 +0100

    Removal of test print

--- sources/signal.c
+++ sources/signal.c
@@ -6,7 +6,7 @@
 /*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:38:36 by nmattos-          #+#    #+#             */
-/*   Updated: 2025/02/26 12:35:52 by nmattos-         ###   ########.fr       */
+/*   Updated: 2025/02/26 13:16:01 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ static void	signal_handler(int signum)
 	if (signum == SIGINT)
 	{
 		g_exit_status = SIGINT_CANCELLATION;
-		printf("m\n");
+		printf("\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
