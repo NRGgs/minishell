c378d65e4144a96af4de584fe34c12a7b6ba27bf
Noah Mattos Oudejans <noahmattosoudejans@icloud.com>
2025-03-13 11:12:07 +0100

    Echo fixed

--- sources/execute/builtins/echo.c
+++ sources/execute/builtins/echo.c
@@ -3,31 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iriadyns <iriadyns@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 13:21:35 by iriadyns          #+#    #+#             */
-/*   Updated: 2025/03/11 13:33:46 by iriadyns         ###   ########.fr       */
+/*   Updated: 2025/03/13 11:11:48 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
+static int	check_for_echo_option(t_command *command, bool *flag);
+
 int	echo(t_command *command, t_shell *shell)
 {
-	int	flag;
+	bool	flag;
 
 	if (!command)
 		return (1);
+	// if (command->options && command->options[0] != '\0')
+	// 	flag = 1;
+	// else
+	// 	flag = 0;
+	flag = false;
+	if (check_for_echo_option(command, &flag) == FAIL)
+		return (1);
 	if (!command->pattern)
 		return (ft_putstr_fd("\n", STDOUT_FILENO), 0);
-	if (command->options && command->options[0] != '\0')
-		flag = 1;
-	else
-		flag = 0;
 	if (prepare_arg(command->env_list, &command->pattern, shell) == FAIL)
 		return (1);
 	ft_putstr_fd(command->pattern, STDOUT_FILENO);
 	if (!flag)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (0);
+}
+
+static int	check_for_echo_option(t_command *command, bool *flag)
+{
+	char	*option;
+	char	*temp;
+	int		i;
+
+	option = command->options;
+	if (!option)
+		return (SUCCESS);
+	if (option[1] == 'n')
+	{
+		i = 1;
+		while (option[i] == 'n')
+			i++;
+		if (option[i] == ' ' || option[i] == '\0')
+		{
+			while (option[i] == ' ')
+				i++;
+			temp = malloc(ft_strlen(option + i) + ft_strlen(command->pattern) + 2);
+			if (!temp)
+				return (FAIL);
+			temp[0] = '\0';
+			ft_strlcat(temp, (option + i), ft_strlen(option + i) + ft_strlen(command->pattern) + 2);
+			ft_strlcat(temp, " ", ft_strlen(option + i) + ft_strlen(command->pattern) + 2);
+			ft_strlcat(temp, command->pattern, ft_strlen(option + i) + ft_strlen(command->pattern) + 2);
+			if (!temp)
+				return (FAIL);
+			free(command->pattern);
+			command->pattern = temp;
+			*flag = true;
+			return (SUCCESS);
+		}
+	}
+	if (!command->pattern)
+		temp = ft_strndup(option, sizeof(option));
+	else
+	{
+		temp = malloc(ft_strlen(option) + ft_strlen(command->pattern) + 2);
+		if (!temp)
+			return (FAIL);
+		temp[0] = '\0';
+		ft_strlcat(temp, option, ft_strlen(option) + ft_strlen(command->pattern) + 2);
+		ft_strlcat(temp, " ", ft_strlen(option) + ft_strlen(command->pattern) + 2);
+		ft_strlcat(temp, command->pattern, ft_strlen(option) + ft_strlen(command->pattern) + 2);
+	}
+	if (!temp)
+		return (FAIL);
+	free(command->pattern);
+	command->pattern = temp;
+	return (SUCCESS);
 }
