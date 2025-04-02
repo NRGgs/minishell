2a68d3a746c0e442129c9faee85fd2ea810d812c
Noah Mattos Oudejans <noahmattosoudejans@icloud.com>
2025-04-02 12:37:17 +0200

    Fix mem leak

--- sources/memory/redirects.c
+++ sources/memory/redirects.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   redirects.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: nmattos <nmattos@student.codam.nl>           +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2025/03/30 12:27:10 by nmattos       #+#    #+#                 */
-/*   Updated: 2025/03/30 13:00:47 by nmattos       ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   redirects.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 12:27:10 by nmattos           #+#    #+#             */
+/*   Updated: 2025/04/02 12:36:03 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ t_redirect	*new_redirect(int type, bool is_input, char *arg)
 		if (!node->arg)
 			return (free(node), NULL);
 	}
+	else
+		node->arg = NULL;
 	node->type = type;
 	node->is_input = is_input;
 	node->next = NULL;
